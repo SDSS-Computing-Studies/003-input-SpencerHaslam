9bd3d4284f8e09675ac2c9df0357c1cc0e939200
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-09-24 11:35:21 -0700

    Update task5.py

--- task5.py
+++ task5.py
@@ -13,3 +13,13 @@
 # Note: You will need to do some strange things with your cube root.
 # Remember that a cube root is the same as an exponent of 1/3, but
 # here you will need to do a power of 1.0/3 or something strange happens.
+import math
+vol = input("Emnter volume")
+x = float(vol)
+z = 4 * math.pi
+a = x / z
+c = 3 * a
+e = 1.0 / 3
+b = c ** e
+y = str(b)
+print("the radius is " + y)
